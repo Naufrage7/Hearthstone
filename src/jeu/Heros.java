c4ce438523953087977d39a0870d4f3e2266eceb
BuildTools <unconfigured@null.spigotmc.org>
2018-03-14 18:45:49 +0100

    Constructeur + getter / setter

--- src/jeu/Heros.java
+++ src/jeu/Heros.java
@@ -1,5 +1,58 @@
 package jeu;
 
 public class Heros {
+	
+	private String nom;
+	private int vie;
+	
+	/**
+	 * Construit un héros à partir de son nom et de sa vie
+	 * @param nom
+	 * @param vie
+	 */
+	public Heros(String nom, int vie) {
+		this.setNom(nom);
+		this.setVie(vie);
+	}
+	
+	/**
+	 * Attribue le nom au héros
+	 * @param nom
+	 */
+	private void setNom(String nom) {
+		if ( nom != null ) {
+			this.nom = nom;
+		} else {
+			// Exception
+		}
+	}
+
+	/**
+	 * Attribue la vie au héros
+	 * @param vie
+	 */
+	private void setVie(int vie) {
+		if ( vie < 0 ) {
+			this.vie = 0;
+		} else {
+			this.vie = vie;
+		}
+	}
+	
+	/**
+	 * Retourne le nom du héros
+	 * @return
+	 */
+	public String getNom() {
+		return this.nom;
+	}
+	
+	/**
+	 * Retourne la vie du héros
+	 * @return
+	 */
+	public int getVie() {
+		return this.vie;
+	}
 
 }

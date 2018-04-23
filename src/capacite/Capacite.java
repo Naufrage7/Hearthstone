5e9b5da4893cfe53bcc9b393523d34a34def62bc
Alexandre Conrad <alexandreconrad57@hotmail.fr>
2018-04-23 11:02:11 +0200

    Commentaires

--- src/capacite/Capacite.java
+++ src/capacite/Capacite.java
@@ -7,15 +7,25 @@ public class Capacite implements ICapacite {
 	private String nom;
 	private String description;
 
+	/***
+	 * Implémentation du constructeur
+	 * 
+	 * @param nom
+	 * @param description
+	 * 
+	 */
 	public Capacite(String nom, String description) {
 		this.setNom(nom);
 		this.setDescription(description);
 	}
 
-	/**
+	/***
 	 * Getter et Setter
+	 * 
+	 * @param nom
+	 * @param description
+	 * @throws IllegalArgumentException
 	 */
-
 	private void setNom(String nom) {
 		if (nom != null) {
 			this.nom = nom;
@@ -42,32 +52,21 @@ public class Capacite implements ICapacite {
 
 	@Override
 	public void executerEffetDebutTour() throws HearthstoneException {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void executerEffetFinTour() throws HearthstoneException {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void executerAction(Object cible) throws HearthstoneException {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void executerEffetMiseEnJeu(Object cible) throws HearthstoneException {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void executerEffetDisparition(Object cible) throws HearthstoneException {
-		// TODO Auto-generated method stub
-
 	}
-
 }

a81f59299b444aa204e535e0a76f565bbe9aeafe
Alexandre Conrad <alexandreconrad57@hotmail.fr>
2018-04-23 19:55:43 +0200

    Modification plateau

--- src/plateau/Plateau.java
+++ src/plateau/Plateau.java
@@ -7,41 +7,41 @@ import joueur.IJoueur;
 
 public class Plateau implements IPlateau {
 
-	private ArrayList<IJoueur> joueur;
+	private ArrayList<IJoueur> joueurs;
+	private IJoueur joueurCourant;
 
 	/**
 	 * Constructeur de la classe
 	 * 
 	 * @param joueur
 	 */
-	public Plateau(ArrayList<IJoueur> joueur) {
-		this.setJoueur(new ArrayList<IJoueur>());
+	public Plateau() {
+		this.setJoueurs(new ArrayList<IJoueur>());
 	}
 
 	/**
 	 * 
 	 * @param joueur
 	 */
-	private void setJoueur(ArrayList<IJoueur> joueur) {
-		if (joueur != null) {
-			this.joueur = joueur;
+	private void setJoueurs(ArrayList<IJoueur> joueurs) {
+		if (joueurs != null) {
+			this.joueurs = joueurs;
 		} else {
 			throw new IllegalArgumentException();
 		}
 	}
 
-	public ArrayList<IJoueur> getJoueur() {
-		return this.joueur;
+	public ArrayList<IJoueur> getJoueurs() {
+		return this.joueurs;
 	}
 
 	/**
-	 * Renvois l'adversaire du joueur courant
 	 * 
 	 * @param IJoueur
 	 */
-	public void setJoueurCourant(IJoueur joueur) throws HearthstoneException {
-		if (joueur != null) {
-			this.joueur = joueur;
+	public void setJoueurCourant(IJoueur joueurs) throws HearthstoneException {
+		if (joueurs.contains(o) == true) {
+			this.joueurs = joueurs;
 		}
 		throw new HearthstoneException();
 	}
@@ -50,18 +50,18 @@ public class Plateau implements IPlateau {
 	 * 
 	 */
 	public IJoueur getJoueurCourant() {
-		return (IJoueur) this.joueur;
+		return (IJoueur) this.joueurs;
 	}
 
 	/**
 	 * Renvoie le joueur courant, c'est-à-dire celui qui a le tour et qui doit
 	 * jouer
 	 */
-	public void ajouterJoueur(IJoueur joueur) throws HearthstoneException {
+	public void ajouterJoueur(IJoueur joueurs) throws HearthstoneException {
 	}
 
 	@Override
-	public IJoueur getAdversaire(IJoueur joueur) throws HearthstoneException {
+	public IJoueur getAdversaire(IJoueur joueurs) throws HearthstoneException {
 		return null;
 	}
 
@@ -75,11 +75,11 @@ public class Plateau implements IPlateau {
 	}
 
 	@Override
-	public void finTour(IJoueur joueur) throws HearthstoneException {
+	public void finTour(IJoueur joueurs) throws HearthstoneException {
 	}
 
 	@Override
-	public void gagnePartie(IJoueur joueur) throws HearthstoneException {
+	public void gagnePartie(IJoueur joueurs) throws HearthstoneException {
 	}
 
 }

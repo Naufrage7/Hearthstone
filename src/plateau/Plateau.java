771e1a11bee2637912b678c5fa0228cbc960eef3
Théo Barragué <naufrage7@gmail.com>
2018-04-23 21:06:43 +0200

    ajout du corps des méthodes

--- src/plateau/Plateau.java
+++ src/plateau/Plateau.java
@@ -7,79 +7,97 @@ import joueur.IJoueur;
 
 public class Plateau implements IPlateau {
 
-	private ArrayList<IJoueur> joueurs;
+	private ArrayList<IJoueur> listeJoueurs;
 	private IJoueur joueurCourant;
+	private boolean partieDemarree;
 
 	/**
-	 * Constructeur de la classe
-	 * 
-	 * @param joueur
+	 * Instancie un nouveau Plateau
 	 */
 	public Plateau() {
-		this.setJoueurs(new ArrayList<IJoueur>());
+		this.setListeJoueurs(new ArrayList<IJoueur>());
+		this.setPartieDemarree(false);
 	}
 
 	/**
+	 * Attribue l'état de la partie
 	 * 
-	 * @param joueur
+	 * @param partieDemarree l'état de la partie
 	 */
-	private void setJoueurs(ArrayList<IJoueur> joueurs) {
-		if (joueurs != null) {
-			this.joueurs = joueurs;
+	private void setPartieDemarree(boolean partieDemarree) {
+		this.partieDemarree = partieDemarree;
+	}
+
+	/**
+	 * Attribue la liste de joueurs
+	 * 
+	 * @param listeJoueurs la liste de joueurs
+	 */
+	private void setListeJoueurs(ArrayList<IJoueur> listeJoueurs) {
+		if (listeJoueurs != null) {
+			this.listeJoueurs = listeJoueurs;
 		} else {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("La liste de joueurs ne peut pas être nulle !");
 		}
 	}
-
+	
+	/**
+	 * Retourne la liste des joueurs
+	 * 
+	 * @return la liste des joueurs
+	 */
 	public ArrayList<IJoueur> getJoueurs() {
-		return this.joueurs;
-	}
-
-	/**
-	 * 
-	 * @param IJoueur
-	 */
-	public void setJoueurCourant(IJoueur joueurs) throws HearthstoneException {
-		if (joueurs.contains(o) == true) {
-			this.joueurs = joueurs;
-		}
-		throw new HearthstoneException();
-	}
-
-	/**
-	 * 
-	 */
-	public IJoueur getJoueurCourant() {
-		return (IJoueur) this.joueurs;
-	}
-
-	/**
-	 * Renvoie le joueur courant, c'est-à-dire celui qui a le tour et qui doit
-	 * jouer
-	 */
-	public void ajouterJoueur(IJoueur joueurs) throws HearthstoneException {
+		return this.listeJoueurs;
 	}
 
 	@Override
-	public IJoueur getAdversaire(IJoueur joueurs) throws HearthstoneException {
-		return null;
+	public void setJoueurCourant(IJoueur joueurCourant) throws HearthstoneException {
+		if ( !this.listeJoueurs.contains(joueurCourant) )
+			throw new HearthstoneException("Le joueur courant doit faire partie de la liste des joueurs du plateau !");
+		this.joueurCourant = joueurCourant;
+	}
+
+	@Override
+	public IJoueur getJoueurCourant() {
+		return this.joueurCourant;
+	}
+
+	@Override
+	public void ajouterJoueur(IJoueur joueur) throws HearthstoneException {
+		if ( this.listeJoueurs.size() >= 2 )
+			throw new HearthstoneException("Il ne peut y avoir que 2 joueurs sur un plateau !");
+		this.listeJoueurs.add(joueur);
+	}
+
+	@Override
+	public IJoueur getAdversaire(IJoueur joueur) throws HearthstoneException {
+		if ( this.listeJoueurs.size() != 2 )
+			throw new HearthstoneException("Il faut deux joueurs pour retourner l'adversaire d'un joueur !");
+		
+		IJoueur adversaire = this.listeJoueurs.get(0);
+		if ( adversaire == this.getJoueurCourant() ) 
+			adversaire = this.listeJoueurs.get(1);
+		
+		return adversaire;
 	}
 
 	@Override
 	public void demarrerPartie() throws HearthstoneException {
+		this.setPartieDemarree(true);
 	}
 
 	@Override
 	public boolean estDemarree() {
-		return false;
+		return this.partieDemarree;
 	}
 
 	@Override
-	public void finTour(IJoueur joueurs) throws HearthstoneException {
+	public void finTour(IJoueur joueur) throws HearthstoneException {
+		this.setJoueurCourant(this.getAdversaire(joueur));
 	}
 
 	@Override
-	public void gagnePartie(IJoueur joueurs) throws HearthstoneException {
+	public void gagnePartie(IJoueur joueur) throws HearthstoneException {
 	}
 
 }

099221bd522025113b34fa31d77dd3f875c78954
Théo Barragué <naufrage7@gmail.com>
2018-04-23 11:21:17 +0200

    ajout du pouvoir au héros

--- src/heros/Heros.java
+++ src/heros/Heros.java
@@ -2,28 +2,31 @@ package heros;
 
 import exception.HearthstoneException;
 import exception.MortException;
+import capacite.Capacite;
 import cible.ICible;
 
 public class Heros implements ICible {
 
 	private String nom;
 	private int vie;
+	private Capacite pouvoir;
 
 	/**
 	 * Construit un héros à partir de son nom et de sa vie
 	 * 
-	 * @param nom
-	 * @param vie
+	 * @param nom le nom du héros
+	 * @param vie la vie du héros
 	 */
-	public Heros(String nom, int vie) {
+	public Heros(String nom, int vie, Capacite pouvoir) {
 		this.setNom(nom);
 		this.setVie(vie);
+		this.setPouvoir(pouvoir);
 	}
 
 	/**
 	 * Attribue le nom au héros
 	 * 
-	 * @param nom
+	 * @param nom le nom du héros
 	 */
 	private void setNom(String nom) {
 		if (nom == null)
@@ -34,18 +37,29 @@ public class Heros implements ICible {
 	/**
 	 * Attribue la vie au héros
 	 * 
-	 * @param vie
+	 * @param vie la vie du héros
 	 */
 	private void setVie(int vie) {
 		if (vie < 0)
 			throw new IllegalArgumentException("La vie du héros ne peut pas être une valeur négative !");
 		this.vie = vie;
 	}
+	
+	/**
+	 * Attribue le pouvoir au héros
+	 * 
+	 * @param pouvoir le pouvoir du héros
+	 */
+	private void setPouvoir(Capacite pouvoir) {
+		if ( pouvoir == null )
+			throw new IllegalArgumentException("Le pouvoir ne peut pas être nul !");
+		this.pouvoir = pouvoir;
+	}
 
 	/**
 	 * Retourne le nom du héros
 	 * 
-	 * @return
+	 * @return le nom du héros
 	 */
 	public String getNom() {
 		return this.nom;
@@ -54,16 +68,24 @@ public class Heros implements ICible {
 	/**
 	 * Retourne la vie du héros
 	 * 
-	 * @return
+	 * @return la vie du héros
 	 */
 	public int getVie() {
 		return this.vie;
+	}
+	
+	/**
+	 * Retourne le pouvoir du héros
+	 * @return le pouvoir du héros
+	 */
+	public Capacite getPouvoir() {
+		return this.pouvoir;
 	}
 
 	/**
 	 * Occasionne des dégats au héros
 	 * 
-	 * @param degats
+	 * @param degats le nombre de dégats à occasionner
 	 * @throws MortException
 	 */
 	public void recevoirDegats(int degats) throws HearthstoneException {

f6084024e8873cb99a97c6c3c136c1148abd7334
Théo Barragué <naufrage7@gmail.com>
2018-04-23 21:38:11 +0200

    javadoc + exceptions

--- src/carte/Serviteur.java
+++ src/carte/Serviteur.java
@@ -10,11 +10,11 @@ public class Serviteur extends Carte {
 	/**
 	 * Constructeur de la classe Serviteur
 	 * 
-	 * @param attaque
-	 * @param vie
-	 * @param nom
-	 * @param cout
-	 * @param proprietaire
+	 * @param attaque l'attaque du serviteur
+	 * @param vie la vie du serviteur
+	 * @param nom le nom du serviteur
+	 * @param cout le cout du serviteur
+	 * @param proprietaire le propriétaire du serviteur
 	 */
 	public Serviteur(int attaque, int vie, String nom, int cout, IJoueur proprietaire) {
 		super(nom, cout, proprietaire);
@@ -23,42 +23,44 @@ public class Serviteur extends Carte {
 	}
 
 	/**
-	 * Test si l'attaque n'est pas égale à 0
+	 * Attribue l'attaque du serviteur
 	 * 
-	 * @param attaque
+	 * @param attaque l'attaque du serviteur
 	 */
 	public void setAttaque(int attaque) {
 		if (attaque >= 0) {
 			this.attaque = attaque;
 		} else {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("L'attaque du serviteur ne peut pas être une valeur négative !");
 		}
 	}
 
 	/**
-	 * Test si la vie n'est pas égale à 0
+	 * Attribue la vie du serviteur
 	 * 
-	 * @param pv
+	 * @param vie la vie du serviteur
 	 */
 	public void setVie(int vie) {
-		if (vie > 0) {
+		if (vie >= 0) {
 			this.vie = vie;
 		} else {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("La vie du serviteur ne peut pas être une valeur négative !");
 		}
 	}
 
 	/**
+	 * Retourne l'attaque du serviteur
 	 * 
-	 * @return attaque
+	 * @return attaque l'attaque du serviteur
 	 */
 	public int getAttaque() {
 		return this.attaque;
 	}
 
 	/**
+	 * Retourne la vie du serviteur
 	 * 
-	 * @return vie
+	 * @return vie la vie du serviteur
 	 */
 	public int getVie() {
 		return this.vie;

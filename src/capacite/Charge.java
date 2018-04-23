e87ef863ede8f3b866961c8feb0ab0830a2892cd
Alexandre Conrad <alexandreconrad57@hotmail.fr>
2018-04-23 11:51:48 +0200

    Création de la classe "Charge"

---
+++ src/capacite/Charge.java
@@ -0,0 +1,14 @@
+package capacite;
+
+public class Charge extends Capacite {
+
+	/**
+	 * 
+	 * @param nom
+	 * @param description
+	 */
+
+	public Charge(String nom, String description) {
+		super(nom, description);
+	}
+}

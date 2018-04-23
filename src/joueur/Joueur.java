7bb41ec9a55da0f3fa3a93b8a46b60018eb6797d
Théo Barragué <naufrage7@gmail.com>
2018-04-23 21:37:02 +0200

    gestion du deck + ajout d'exceptions

--- src/joueur/Joueur.java
+++ src/joueur/Joueur.java
@@ -3,6 +3,7 @@ package joueur;
 import java.util.ArrayList;
 
 import carte.ICarte;
+import carte.Serviteur;
 import exception.HearthstoneException;
 import heros.Heros;
 
@@ -14,6 +15,7 @@ public class Joueur implements IJoueur {
 	private int stockMana;
 	private ArrayList<ICarte> main;
 	private ArrayList<ICarte> jeu;
+	private ArrayList<ICarte> deck;
 
 	/**
 	 * Instancie un Joueur
@@ -28,6 +30,7 @@ public class Joueur implements IJoueur {
 		this.setStockMana(0);
 		this.setMain(new ArrayList<ICarte>());
 		this.setJeu(new ArrayList<ICarte>());
+		this.initialiserDeck();
 	}
 
 	/**
@@ -84,6 +87,9 @@ public class Joueur implements IJoueur {
 	 * @param main la main du joueur
 	 */
 	private void setMain(ArrayList<ICarte> main) {
+		if ( main == null ) 
+			throw new IllegalArgumentException("La main du joueur ne peut pas être nulle !");
+		
 		this.main = main;
 	}
 	
@@ -93,7 +99,34 @@ public class Joueur implements IJoueur {
 	 * @param jeu le jeu du joueur
 	 */
 	private void setJeu(ArrayList<ICarte> jeu) {
+		if ( jeu == null ) 
+			throw new IllegalArgumentException("Le jeu du joueur ne peut pas être nul !");
+		
 		this.jeu = jeu;
+	}
+	
+	private void setDeck(ArrayList<ICarte> deck) {
+		if ( main == null ) 
+			throw new IllegalArgumentException("Le deck du joueur ne peut pas être nul !");
+		
+		this.deck = deck;
+	}
+	
+	private ArrayList<ICarte> getDeck() {
+		return this.deck;
+	}
+	
+	private void initialiserDeck() {
+		this.setDeck(new ArrayList<ICarte>());
+		this.deck.add(new Serviteur(2, 1, "Chasse-marée murloc", 2, this));
+		this.deck.add(new Serviteur(6, 6, "Champion de Hurlevent", 7, this));
+		this.deck.add(new Serviteur(2, 2, "Chef de raid", 3, this));
+		this.deck.add(new Serviteur(5, 4, "Garde de Baie-du-butin", 5, this));
+		this.deck.add(new Serviteur(5, 2, "La missilière téméraire", 6, this));
+		this.deck.add(new Serviteur(4, 4, "L'orgre-magi", 4, this));
+		this.deck.add(new Serviteur(4, 7, "Archimage", 6, this));
+		this.deck.add(new Serviteur(1, 1, "Gnôme lépreux", 1, this));
+		this.deck.add(new Serviteur(2, 3, "Golem des moissons", 3, this));
 	}
 	
 	@Override
@@ -165,6 +198,11 @@ public class Joueur implements IJoueur {
 
 	@Override
 	public void piocher() throws HearthstoneException {
+		if ( this.deck.size() == 0 )
+			throw new HearthstoneException("Le deck est vide malheureux !");
+		
+		ICarte carte = this.deck.remove(0);
+		this.main.add(carte);
 	}
 
 	@Override

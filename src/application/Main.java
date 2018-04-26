fb352ff5e5f91b43b35ef4eb12f47eaa4d5da994
Théo Barragué <naufrage7@gmail.com>
2018-04-26 18:38:26 +0200

    créatio du corps de base

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,27 @@
 package application;
 
+import capacite.Capacite;
+import exception.HearthstoneException;
+import heros.Heros;
+import joueur.Joueur;
+import plateau.Plateau;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world !");
-		System.exit(0);
+		Plateau plateau = Plateau.getInstance();
+		
+		Joueur j1 = new Joueur("Théo", new Heros("Jaina", 15, new Capacite("Ne rien faire", "Ne fait rien")));
+		Joueur j2 = new Joueur("Alex", new Heros("Rexxor", 15, new Capacite("Ne rien faire", "Ne fait rien")));
+		
+		try {
+			plateau.ajouterJoueur(j1);
+			plateau.ajouterJoueur(j2);
+		} catch ( HearthstoneException e ) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(plateau);
 	}
 
 }

780d6216e87d7dce710c02b0991dfb1eded9e0af
Théo Barragué <naufrage7@gmail.com>
2018-04-27 17:43:55 +0200

    Test si la cible est une instance de Serviteur

--- src/capacite/Charge.java
+++ src/capacite/Charge.java
@@ -16,6 +16,9 @@ public class Charge extends Capacite {
 	 * Permet au serviteur d'attaquer s'il dispose de la capacité charge
 	 */
 	public void executerEffetMiseEnJeu(Object cible) throws HearthstoneException {
+		if ( !(cible instanceof Serviteur) ) 
+			throw new HearthstoneException("La cible ne peut être qu'un serviteur !");
+		
 		Serviteur s = (Serviteur) cible;
 		s.setPeutAttaquer(true);
 	}

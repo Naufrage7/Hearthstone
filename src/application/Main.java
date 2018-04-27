db8e13b580fe00b9f71bfa2c099a17bb9cd796a2
Théo Barragué <naufrage7@gmail.com>
2018-04-27 17:19:53 +0200

    Mise à jour

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,7 @@
 package application;
 
 import capacite.Capacite;
+import carte.ICarte;
 import exception.HearthstoneException;
 import heros.Heros;
 import joueur.Joueur;
@@ -17,6 +18,42 @@ public class Main {
 		try {
 			plateau.ajouterJoueur(j1);
 			plateau.ajouterJoueur(j2);
+			
+			plateau.demarrerPartie();
+			
+			for ( int i = 0; i < 6; i++ ) {	
+				Joueur joueurCourant = (Joueur) plateau.getJoueurCourant();
+				System.out.println("==== " + joueurCourant.getPseudo() + " ====");
+				
+				joueurCourant.prendreTour();
+				
+				ICarte carteAJouer = joueurCourant.getMain().get(0);
+				if ( carteAJouer.getCout() <= joueurCourant.getMana() )
+					joueurCourant.jouerCarte(carteAJouer);
+				
+				System.out.println("Carte(s) en main :");
+				if ( joueurCourant.getMain().isEmpty() ) {
+					System.out.println(" - ( Rien )");
+				} else {
+					for ( ICarte c : joueurCourant.getMain() ) {
+						System.out.println(" - ( " + c.getCout() + " ) " + c.getNom());
+					}
+				}
+				
+				System.out.println("Carte(s) en jeu :");
+				if ( joueurCourant.getJeu().isEmpty() ) {
+					System.out.println(" - ( Rien )");
+				} else {
+					for ( ICarte c : joueurCourant.getJeu() ) {
+						System.out.println(" - " + c.getNom());
+					}
+				}
+				
+				joueurCourant.finirTour();
+				
+				plateau.setJoueurCourant(plateau.getAdversaire(joueurCourant));
+				System.out.println("");
+			}
 		} catch ( HearthstoneException e ) {
 			e.printStackTrace();
 		}
